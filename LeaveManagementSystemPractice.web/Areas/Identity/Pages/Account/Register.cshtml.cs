6dca7726615c6e941b7146cb74552db3b12e55fb
Roman Bits <romanbits2@gmail.com>
2025-07-28 00:13:58 +0300

    Add first name, last name, and date of birth fields to user registration

--- LeaveManagementSystemPractice.web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LeaveManagementSystemPractice.web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -98,6 +98,21 @@ namespace LeaveManagementSystemPractice.web.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+            
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            
+            [Required]
+            [DataType(DataType.Date)]
+            [Display(Name = "Date Of Birth")]
+            public DateOnly DateOfBirth { get; set; }
         }
 
 
@@ -117,6 +132,10 @@ namespace LeaveManagementSystemPractice.web.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                user.DateOfBirth = Input.DateOfBirth;
+                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
